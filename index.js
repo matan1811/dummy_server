4b488c8cf61c5412137af1a578da79381f99321f
Daniel Shir <daniels@viber.com>
2017-05-24 09:06:18 +0300

    Added search request

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-
+const util = require('util');
 
 const port = process.env.PORT || 8081;
 let app = express();
 
 app.get('/', (req, res) => {
 	res.end("hello");
+});
+
+app.get('/v1/search', (req, res) => {
+	console.log(`Got search request - ${util.inspect(req.query)}`);
+
+	res.json({
+		"name": "some name",
+		"id" : 3934937498,
+		"level": 4,
+		"profileImage" : "https://api.adorable.io/avatars/285/3934937498",		
+	})
+	.end();
 });
 
 app.listen(port, () => {
